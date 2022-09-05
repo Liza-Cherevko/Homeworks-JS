994dfe52416b7f9ae59c4d4f44211db38eae3319
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-05 16:24:20 +0300

    commit

--- HW-4/HW-4.js
+++ HW-4/HW-4.js
@@ -17,8 +17,10 @@
 
 let action = getAction()
 let numberOfvalue = getNumbers('Put numbers of value')
+let value = showNum('Put operand')
+let result = calculate( action)
+showResult(action, result)
 
-let value = getValue('Put value')
 
 function getAction(){ 
     let action = prompt('Put action')
@@ -34,32 +36,103 @@ function getActionInvalid(action) {
 }
 
 function getNumbers(title) { 
-    let operand = prompt(title);
-    while (getNumbersInvalid(operand)) { 
-        operand = prompt('Put number of values again')
+    let number = prompt(title);
+    while (getNumbersInvalid(number)) { 
+        number = prompt('Put number of values again')
     }
-    return +operand
+    return +number
 }
 
-function getNumbersInvalid (operand) { 
+function getNumbersInvalid (number) { 
+    return number === null || number.trim() === '' || isNaN(number);
+}
+function getOperandInvalid (operand) { 
     return operand === null || operand.trim() === '' || isNaN(operand);
 }
 
-function showNum (){
-    let num = '0'
-    
-    for (let i = 0; i <= numberOfvalue.length; i++) { 
-        if (i === numberOfvalue.length) { 
-           
-        }
-    }   
-   
-}
-
-function getValue(title) { 
+function showNum (title){
     let operand = prompt(title)
-    while (getNumbersInvalid(operand)) { 
-        operand = prompt('Put  values again')
+    for (let i = 0 ; i <= numberOfvalue; i++) { 
+        if (operand !== numberOfvalue ) { 
+            operand = prompt(title)
+        }
+    }
+    while (getOperandInvalid(operand)) { 
+        operand = prompt('Put operand again')
     }
     return +operand
 }
+
+function calculate(action){
+    switch (action){
+        case '+':
+            return operand[i] + operand[i];
+        case '-':
+            return operand[i] - operand[i];
+        case '*':
+           return operand[i] * operand[i];
+        case '/':
+            return operand[i] / operand[i];
+         default:
+            alert('Something wrong')       
+    }
+}
+function showResult( action, result) {
+    alert(`${operand[i]}  ${action} ${operand[i]}   = ${result}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getCalculate( action){
+//     switch (action){
+//         case '+':
+//             return  operand + operand;
+//         case '-':
+//             return operand - operand;
+//         case '*':
+//             return operand * operand;
+//         case '/':
+//             return operand / operand;
+//          default:
+//             alert('Something wrong')       
+//     }
+// }
+
+// function showResult (){
+//     let sum = 0,
+//         sub = 0,
+//         mul = 0,
+//         div = 0;
+    
+//     for (let i = 0; i <= numberOfvalue; i++) {
+//         if (action == '+') {
+//             sum = `${i} + ${i}`;
+           
+//         } else if (action == '-') {
+//             sub = `${i} - ${i}`;
+//         } else if (action == '*') {
+//             mul = `${i} * ${i}`;
+//         } else { 
+//             div = `${i} / ${i}`;
+//         }
+//     }
+
+//     alert( `${i} ${action} ${i} = ${result} `);
+// }
