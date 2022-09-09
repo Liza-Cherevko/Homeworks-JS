cd0c896d3dece8f18dfd30f87511ce353c95cc0e
Liza <cherevkoelp@gmail.com>
2022-09-09 20:18:10 +0300

    commit

--- HW-5/HW-5.js
+++ HW-5/HW-5.js
@@ -4,12 +4,14 @@
 
 
 
-let number;
+
+
+let operand;
 let array = []
 let action = getAction()
-
 let value = getOperands()
 calculate()
+
 function getAction(){ 
     let action = prompt('Put action')
     
@@ -35,88 +37,100 @@ function getOperands(title) {
          return array;
  }
 
- function calculate() { 
+
+function calculate() { 
     let condition;
-    switch (action) { 
-        case '+':
-            condition= array.map(i=>x+=i, x=0).reverse()[0]
-            break;
-         case '-':
-            condition = array.reduce((acc, rec) => acc - rec);
-            break;
-            case '*':
-                condition = array.reduce((acc, rec) => acc * rec);
-            break;
-            case '/':
-                condition = array.reduce((acc, rec) => acc / rec);
-               break;
+    let str = '';
+    for (let i = 0; i < value.length; i++) {
+        if (i === 0) {
+            condition = value
+            str += ` ${value[0]} `;
+        }
+        else {
+            {  
+            switch (action) {
+                case '+':
+                    condition = array.map(i => x += i, x = 0).reverse()[0]
+                    str +=  `+ ${value[i]}`
+                    break;
+                case '-':
+                    condition = array.reduce((acc, rec) => acc - rec);
+                    str += `- ${value[i]}`;
+                    break;
+                case '*':
+                    condition = array.reduce((acc, rec) => acc * rec);
+                    str += `* ${value[i]}`;
+                    break;
+                case '/':
+                    condition = array.reduce((acc, rec) => acc / rec);
+                    str += `/ ${value[i]}`;
+                    break;
+            }
+        } 
     }
-    console.log(condition)
-   alert(condition)
-   
-}    
- 
-
-
- // let operand;
-// let action = getAction();
-// getOperands();
-
-
-// function getAction() { 
-//     let action = prompt('Put some action: +, -, *, /')
-//     console.log(action)
+}
     
-//     while (isActionInvalid(action)) { 
-//         action =  prompt('Put  action again: +, -, *, /')
-//     }
-//     return action
-// }
-// function isActionInvalid(action) { 
-//     return action !== '+' && action !== '-' && action !== '*' && action !== '/';
-// }
+    let result = ` ${str} = ${condition}`;
+    console.log(result)
+    alert(result)
+ }
 
-
-// function isOperandValidation(operand){ 
-//     return operand == null || operand == '';
-// }
-
-// function getOperands (){
-//      operand = prompt('Put operands ,').split(',');
-//      console.log('operands value', operand)
-//      while (isOperandValidation(operand)) {
-//         operand = prompt('Put operand again')
-//     }
-//     return operand
-// }
+   
  
-// function operandCalculation() {
-// let acc = operand
+ 
+// первая версия, выводит только результат
 
-//             switch (action) {
-      
-//                 case '+':
-//                     operand.map(i=>x+=i, x=0).reverse()[0]
-//                     break;
-//                 case '-':
-//                     acc = acc - operand;
-//                     str += `- ${operand}`;
-//                     break;
-//                 case '*':
-//                     acc = acc * operand;
-//                     str += `* ${operand}`;
-//                     break;
-//                 case '/':
-//                     acc = acc / operand;
-//                     str += `/ ${operand}`;
-//                     break;
-//                 default:
-//                     alert('Something wrong')
-//             }
-        
-            
-// }
-
+//  let number;
+//  let array = []
+//  let action = getAction()
+ 
+//  let value = getOperands()
+//  calculate()
+//  function getAction(){ 
+//      let action = prompt('Put action')
+     
+//      while (getActionInvalid(action)) { 
+//          action = prompt('Put action again');
+//      }
+//      console.log(action)
+//      return action
+//  }
+//  function getActionInvalid(action) { 
+//      return action !== '+' && action !== '-' && action !== '*' && action !== '/';
+//  }
+ 
+//  function getNumbersInvalid (operand) { 
+//      return operand === null || operand === '' ;
+//  }
+ 
+//  function getOperands(title) {
+//      operand = prompt('Put operands ,').split(',');
+//      console.log( 'operands', operand)
+//      array = operand.map(i=>Number(i));
+ 
+//           return array;
+//   }
+ 
+//   function calculate() { 
+//      let condition;
+//      switch (action) { 
+//          case '+':
+//              condition= array.map(i=>x+=i, x=0).reverse()[0]
+//              break;
+//           case '-':
+//              condition = array.reduce((acc, rec) => acc - rec);
+//              break;
+//              case '*':
+//                  condition = array.reduce((acc, rec) => acc * rec);
+//              break;
+//              case '/':
+//                  condition = array.reduce((acc, rec) => acc / rec);
+//                 break;
+//      }
+//      console.log(condition)
+//     alert(condition)
+    
+//  }  
 
 
 
